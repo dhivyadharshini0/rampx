d4a9a2145947f4c2e2894d865f11c59b19605b5a
dhivyadharshini0 <149153797+dhivyadharshini0@users.noreply.github.com>
2025-05-17 11:07:07 +0530

    Day 5

---
+++ Day 5/reverse2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num = 23456,rev = 0;
+
+    while (num != 0) {
+        rev = rev * 10 + num % 10;  
+        num = num / 10;             
+    }
+
+    printf("Reversed number = %d\n", rev);
+
+    return 0;
+}
